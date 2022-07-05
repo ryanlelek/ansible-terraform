939e5b78153b85e641e6d3853166a6c33f15f211
Ryan Lelek <ryanlelek@gmail.com>
2022-07-05 05:16:13 -0500

    Edit: Upgrading Terraform modules

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This will show some of the intermediate skills for DevOps for learning.
 1. Edit `./env.sh` with DigitalOcean API key and edit SSH key filepath if needed.
 2. Run `source ./env.sh` to load environment variables
 3. Edit `provider.tf` bottom to the SSH Key name you added to DigitalOcean.
-4. Run `terraform init` to install dependencies.
+4. Run `terraform init -upgrade` to install dependencies.
 5. Run `terraform apply` and it will create with Terraform and provision with Ansible.
 6. Check the `./inventory` file for your loadbalancer ip. Visit this in curl or a browser and see the traffic flip-flop between web servers.
 7. By default, there are 2 web servers. Edit `count` in `web.tf` file to a higher number to test further if desired.
--- provider.tf
+++ provider.tf
@@ -3,19 +3,22 @@ terraform {
     # https://registry.terraform.io/providers/hashicorp/local/latest/docs
     hashicorplocal = {
       source  = "hashicorp/local"
-      version = "2.2.2"
+      version = "2.2.3"
     }
+    # https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "2.19.0"
+      version = "2.21.0"
     }
+    # https://registry.terraform.io/providers/linode/linode/latest/docs
     #linode = {
     #  source  = "linode/linode"
-    #  version = "1.27.1"
+    #  version = "1.28.0"
     #}
+    # https://registry.terraform.io/providers/vultr/vultr/latest/docs
     #vultr = {
     #  source  = "vultr/vultr"
-    #  version = "2.10.1"
+    #  version = "2.11.3"
     #}
   }
 }
